06c97b936a40e9b88699b0a6ce4a370835df1aad
Petr Krejcik <p.krejcik@stuart.com>
2023-08-16 14:21:29 +0200

    init

---
+++ src/Subpage.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {
+  Link
+} from "react-router-dom";
+
+function Subpage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Subpage
+      </header>
+    </div>
+  );
+}
+
+export default Subpage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Subpage from "./Subpage";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/subpage" element={<Subpage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
